7d6552923d2e4b138a54ed4f8ef30df5632b38ee
pmcgrath <pmcgrath@gmail.com>
2019-11-14 12:46:21 +0000

    Updated to include short commit

--- promotion-manager.sh
+++ promotion-manager.sh
@@ -5,21 +5,29 @@ set -e
 promote-using-a-pr() {
 	env=${1}
 	source_commit=${2}
-	image_digest=${3}
+	source_short_commit=${3}
+	image_digest=${4}
 
-	git branch -b "promote-to-$env-for-source-commit-$commit"
-	commit-with-env-values $env ${source_commit} ${image_digest}
-	git set
+	initial_branch=$(git rev-parse --abbrev-ref HEAD)
+	pr_branch="promote-to-${env}-for-source-commit-${source_short_commit}"
+
+	git checkout -b ${pr_branch}
+	commit-with-env-values $env ${source_commit} ${source_short_commit} ${image_digest}
+	git push --set-upstream origin ${pr_branch}
 	git push
+
 	# Create PR
+
+	git checkout ${initial_branch}
 }
 
 promote-on-current-branch() {
 	env=${1}
 	source_commit=${2}
-	image_digest=${3}
+	source_short_commit=${3}
+	image_digest=${4}
 
-	commit-with-env-values $env ${source_commit} ${image_digest}
+	commit-with-env-values $env ${source_commit} ${source_short_commit} ${image_digest}
 	git push
 }
 
@@ -28,11 +36,12 @@ promote-on-current-branch() {
 commit-with-env-values() {
 	env=${1}
 	source_commit=${2}
-	image_digest=${3}
+	source_short_commit=${3}
+	image_digest=${4}
 
 	values_file_name=$(get-env-values-file-name ${env})
 
-	update-env-values-file ${values_file_name} ${source_commit} ${image_digest}
+	update-env-values-file ${values_file_name} ${source_commit} ${source_short_commit} ${image_digest}
 
 	commit_message="Promoted values in ${values_file_name} on source commit ${source_commit}"
 	git commit -am "${commit_message}"
@@ -45,12 +54,14 @@ get-env-values-file-name() {
 update-env-values-file() {
 	file_name=${1}
 	source_commit=${2}
-	image_digest=${3}
+	source_short_commit=${3}
+	image_digest=${4}
 
 	# Crude but good enough for here
 	sed_arg=
 	sed_arg+="s/^imageDigest:.*/imageDigest: ${image_digest}/; "
 	sed_arg+="s/^sourceCommit:.*/sourceCommit: ${source_commit}/; "
+	sed_arg+="s/^sourceShortCommit:.*/sourceShortCommit: ${source_short_commit}/; "
 
 	sed -i "${sed_arg}" ${file_name}
 }
